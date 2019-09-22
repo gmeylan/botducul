a31feb9893c337034b317468317e2c060394820d
etienne-napoleone <etienne@terra.money>
2019-09-22 17:17:41 +0900

    style: comply with PEP8
    Using black formatter

--- app/bot.py
+++ app/bot.py
@@ -7,19 +7,28 @@ token = os.getenv("DISORD_TOKEN")
 
 client = discord.Client()
 
-async def seoul_swiss_time(message):
-        utc = arrow.utcnow()
 
-        swiss_time = utc.to('Europe/Zurich')
-        seoul_time = utc.to('Asia/Seoul')
-        swiss_formatted_time = swiss_time.format('HH:mm:ss')
-        korean_formatted_time = seoul_time.format('HH:mm:ss')
-        response = 'Il est ' + swiss_formatted_time + ' en Suisse\nEt ' + korean_formatted_time + ' en Corée (pas du nord) !'
-        await message.channel.send(response)
+async def seoul_swiss_time(message):
+    utc = arrow.utcnow()
+
+    swiss_time = utc.to("Europe/Zurich")
+    seoul_time = utc.to("Asia/Seoul")
+    swiss_formatted_time = swiss_time.format("HH:mm:ss")
+    korean_formatted_time = seoul_time.format("HH:mm:ss")
+    response = (
+        "Il est "
+        + swiss_formatted_time
+        + " en Suisse\nEt "
+        + korean_formatted_time
+        + " en Corée (pas du nord) !"
+    )
+    await message.channel.send(response)
+
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f"{client.user} has connected to Discord!")
+
 
 @client.event
 async def on_message(message):
@@ -27,12 +36,13 @@ async def on_message(message):
         return
 
     for mention in message.mentions:
-        if str(mention) == 'gasegg#7082':
+        if str(mention) == "gasegg#7082":
             await seoul_swiss_time(message)
-        if str(mention) == 'botducul#3369':
+        if str(mention) == "botducul#3369":
             await message.channel.send("Je suis ton maître !")
 
-    if message.content == '/time':
+    if message.content == "/time":
         await seoul_swiss_time(message)
-    
+
+
 client.run(token)
